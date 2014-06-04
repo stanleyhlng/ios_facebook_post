42303c23f3bb9c927ff12616f40bd7a0e545cbef
Stanley Ng <stanleyhlng@yahoo.com.hk>
2014-06-04 02:28:33 -0700

    Initial Commit

---
+++ ios_facebook_post/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios_facebook_post
+//
+//  Created by Stanley Ng on 6/4/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios_facebook_postTests/ios_facebook_postTests.m
@@ -0,0 +1,34 @@
+//
+//  ios_facebook_postTests.m
+//  ios_facebook_postTests
+//
+//  Created by Stanley Ng on 6/4/14.
+//  Copyright (c) 2014 Stanley Ng. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios_facebook_postTests : XCTestCase
+
+@end
+
+@implementation ios_facebook_postTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
